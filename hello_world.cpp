71fe563b04c1e9861394a7879ea392134aa7a91b
jawnsunn <jawnsunn@gmail.com>
2016-09-02 00:04:10 -0700

    comment hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-	cout << "Hello World!" << endl;
+	cout << "Hello World!" << endl; //this section lets the command line terminal write "Hello World!"
 	return 0;
 }
